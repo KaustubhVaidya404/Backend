f5998a8e69a47dbc71f67a39d74b8725c6f5dedf
Kaustubh <pro.vaidya4413@gmail.com>
2024-01-13 14:04:19 +0530

    CR operations on jobs complete

--- src/controllers/jobs.controllers.js
+++ src/controllers/jobs.controllers.js
@@ -1,37 +1,55 @@
 
 import db from "../db/firestore.js";
 
-const docRef = db.collection('jobs');
-const getdoc = await docRef.get();
+const docRef = await db.collection('jobs');
 
 const getAllJobs = async (req, res)=>{
-    if(getdoc.exists){
-        const data = getdoc.data();
-        res.json({
-            success: true,
-            data
-        })
-    } else {
-        res.json({ success: false, message: 'Document not found' });
+    try{
+        const jobRef = docRef;
+        const getresponse = await jobRef.get();
+        let responseArr = [];
+        getresponse.forEach(doc => {
+            responseArr.push(doc.data())
+        });
+        res.send(responseArr);
+    } catch(e) {
+        res.send({error: e})
     }
 };
+
+const getByJobTitle = async (req, res) => {
+    try{
+        const jobRef = docRef.doc(req.params.id);
+        const getresponse = await jobRef.get();
+        res.send(getresponse.data());
+    } catch(e) {
+        res.send({error: e})
+    }
+}
 
 
 const postJob = async (req,res) => {
     try{
+        let rannum = Math.random();
+        let rawtitlestr = req.body.jobtitle;
+        let titlestr = rawtitlestr.split(" ").join("");
+        const id = rannum+"|"+titlestr;
         const jobJson = {
             jobtitle: req.body.jobtitle,
             jobdesc: req.body.jobdesc,
-            jobskills: req.body.jobskills,
+            orgname: req.body.orgname,
             jobduration: req.body.jobduration,
             jobstipend: req.body.jobstipend,
+            skills: req.body.skills,
+            perks: req.body.perks,
+            noofopenings: req.body.noofopenings,
             postdate: req.body.postdate
         };
-        const response = docRef.add(jobJson);
+        const response = docRef.doc(id).set(jobJson);
         req.send(response)
     } catch (error) {
         res.send(error);
     }
 };
 
-export {getAllJobs, postJob}
+export {getAllJobs, postJob, getByJobTitle}
--- src/routes/jobs.routes.js
+++ src/routes/jobs.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getAllJobs, postJob } from "../controllers/jobs.controllers.js";
+import { getAllJobs, postJob, getByJobTitle } from "../controllers/jobs.controllers.js";
 
 const router = Router();
 
 router.route('/').get(getAllJobs);
 router.route('/createopening').post(postJob);
-
+router.route('/:id').get(getByJobTitle);
 
 export default router;
